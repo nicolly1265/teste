09c51d49d0bad60f5aa27044889f938fe151a21c
nicolly1265 <nicolly.santos.ribeiro@escola.pr.gov.br>
2026-02-12 08:52:54 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,11 @@
+console.log("hello world!");
+console.log(0123456789);
+console.log("nicolly");
+x=8+9;
+console.log(x);
+y=81;
+z=9;
+t=y+z;
+console.log(t);
+console.log("o resultado é:",t);
+console.log("o outro resultado é:",x);
